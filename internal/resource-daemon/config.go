4f3c94e26dda008463c30fb46821d59499b224fc
cali4888 <pisarukvadim@gmail.com>
2019-04-05 17:24:25 +0300

    Typo

--- internal/resource-daemon/config.go
+++ internal/resource-daemon/config.go
@@ -1,8 +1,8 @@
 package resource_daemon
 
 // NodeConfig contains SLURM cluster local/ssh address.
-// that config is used for file which resource-daemon creates on each k8s node
-// job-companion uses that addresses from file for commenting with slurm
+// NodeConfig is used for the file which resource-daemon creates on each k8s node.
+// Job-companion uses addresses from the file for communicating with SLURM cluster.
 type NodeConfig struct {
 	SSHAddr   string `yaml:"ssh_addr"`
 	LocalAddr string `yaml:"local_add"`
