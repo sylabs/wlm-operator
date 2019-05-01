7c833564f342cab6a18055fb02825261b70b3d2d
sashayakovtseva <sasha@sylabs.io>
2019-05-01 16:30:19 +0300

    Generate go file based on proto

--- generate.go
+++ generate.go
@@ -18,3 +18,4 @@ package slurm_operator
 //go:generate go run vendor/k8s.io/code-generator/cmd/deepcopy-gen/main.go -O zz_generated.deepcopy -i ./pkg/operator/apis/slurm/v1alpha1 -h COPYRIGHT
 //go:generate go run vendor/k8s.io/kube-openapi/cmd/openapi-gen/openapi-gen.go -o pkg/operator/apis/slurm -O zz_generated.openapi -p v1alpha1 -i ./pkg/operator/apis/slurm/v1alpha1 -h COPYRIGHT
 //go:generate go run vendor/sigs.k8s.io/controller-tools/cmd/controller-gen/main.go crd --output-dir deploy/crds --apis-path pkg/operator/apis
+//go:generate protoc --go_out=plugins=grpc:. pkg/workload/api/workload.proto
