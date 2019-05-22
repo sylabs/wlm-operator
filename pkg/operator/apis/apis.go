aaa521526f16c7e726fee33ed27cf8602a0e677a
sashayakovtseva <sasha@sylabs.io>
2019-05-22 12:10:20 +0300

    Simplify api registration

--- pkg/operator/apis/apis.go
+++ pkg/operator/apis/apis.go
@@ -15,11 +15,17 @@
 package apis
 
 import (
+	"github.com/sylabs/slurm-operator/pkg/operator/apis/slurm/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // AddToSchemes may be used to add all resources defined in the project to a Scheme
 var AddToSchemes runtime.SchemeBuilder
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+}
 
 // AddToScheme adds all Resources to the Scheme
 func AddToScheme(s *runtime.Scheme) error {
