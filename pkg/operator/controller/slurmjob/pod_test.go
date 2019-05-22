bbcd34836edfa3549d09370bc7a488a0e7e88176
sashayakovtseva <sasha@sylabs.io>
2019-05-22 12:11:07 +0300

    Configure slurm job pod affinity

---
+++ pkg/operator/controller/slurmjob/pod_test.go
@@ -0,0 +1,161 @@
+// Copyright (c) 2019 Sylabs, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slurmjob
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/sylabs/slurm-operator/pkg/operator/apis/slurm/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestAffinityForSj(t *testing.T) {
+	tt := []struct {
+		name           string
+		sj             *v1alpha1.SlurmJob
+		expectAffinity *corev1.Affinity
+		expectError    bool
+	}{
+		{
+			name: "invalid batch script",
+			sj: &v1alpha1.SlurmJob{
+				Spec: v1alpha1.SlurmJobSpec{
+					Batch: `
+#!/bin/sh
+#SBATCH --time=foo
+srun singularity pull -U library://sylabsed/examples/lolcow
+srun singularity run lolcow_latest.sif
+srun rm lolcow_latest.sif
+`,
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "wall time affinity",
+			sj: &v1alpha1.SlurmJob{
+				Spec: v1alpha1.SlurmJobSpec{
+					Batch: `
+#!/bin/sh
+#SBATCH --time=00:05:00
+srun singularity pull -U library://sylabsed/examples/lolcow
+srun singularity run lolcow_latest.sif
+srun rm lolcow_latest.sif
+`,
+				},
+			},
+			expectAffinity: &corev1.Affinity{
+				NodeAffinity: &corev1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+						NodeSelectorTerms: []corev1.NodeSelectorTerm{
+							{
+								MatchExpressions: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "slurm.sylabs.io/time-limit",
+										Operator: "Gt",
+										Values:   []string{"299"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "nodes affinity",
+			sj: &v1alpha1.SlurmJob{
+				Spec: v1alpha1.SlurmJobSpec{
+					Batch: `
+#!/bin/sh
+#SBATCH -N 7
+
+srun singularity pull -U library://sylabsed/examples/lolcow
+srun singularity run lolcow_latest.sif
+srun rm lolcow_latest.sif
+`,
+				},
+			},
+			expectAffinity: &corev1.Affinity{
+				NodeAffinity: &corev1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+						NodeSelectorTerms: []corev1.NodeSelectorTerm{
+							{
+								MatchExpressions: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "slurm.sylabs.io/nodes",
+										Operator: "Gt",
+										Values:   []string{"6"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "full affinity",
+			sj: &v1alpha1.SlurmJob{
+				Spec: v1alpha1.SlurmJobSpec{
+					Batch: `
+#!/bin/sh
+#SBATCH -N 7
+#SBATCH --time=1-00:05:00
+
+srun singularity pull -U library://sylabsed/examples/lolcow
+srun singularity run lolcow_latest.sif
+srun rm lolcow_latest.sif
+`,
+				},
+			},
+			expectAffinity: &corev1.Affinity{
+				NodeAffinity: &corev1.NodeAffinity{
+					RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+						NodeSelectorTerms: []corev1.NodeSelectorTerm{
+							{
+								MatchExpressions: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "slurm.sylabs.io/nodes",
+										Operator: "Gt",
+										Values:   []string{"6"},
+									},
+									{
+										Key:      "slurm.sylabs.io/time-limit",
+										Operator: "Gt",
+										Values:   []string{"86699"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := affinityForSj(tc.sj)
+			if tc.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.Equal(t, tc.expectAffinity, actual)
+		})
+	}
+}
