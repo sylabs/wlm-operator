3ce155d679f1bce786cdc620f2d10cba8309eb9f
sashayakovtseva <sasha@sylabs.io>
2019-05-29 19:25:52 +0300

    Fix vendor

--- pkg/operator/apis/slurm/v1alpha1/register.go
+++ pkg/operator/apis/slurm/v1alpha1/register.go
@@ -33,3 +33,8 @@ var (
 
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource.
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
