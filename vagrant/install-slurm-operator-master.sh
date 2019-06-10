eb920251464e3e86eda7717455ea25ad01174843
sashayakovtseva <sasha@sylabs.io>
2019-06-10 15:00:19 +0300

    Remove unused slurm config

--- vagrant/install-slurm-operator-master.sh
+++ vagrant/install-slurm-operator-master.sh
@@ -3,8 +3,6 @@
 GOPATH="${HOME}/go"
 SINGULARITY_SLURM_OPERATOR_REPO="github.com/sylabs/slurm-operator"
 
-kubectl apply -f /sync/etc/slurm-operator-cfg.yaml
-
 kubectl apply -f ${GOPATH}/src/${SINGULARITY_SLURM_OPERATOR_REPO}/deploy/crds/slurm_v1alpha1_slurmjob.yaml
 kubectl apply -f ${GOPATH}/src/${SINGULARITY_SLURM_OPERATOR_REPO}/deploy/operator-rbac.yaml
 kubectl apply -f ${GOPATH}/src/${SINGULARITY_SLURM_OPERATOR_REPO}/deploy/operator.yaml
