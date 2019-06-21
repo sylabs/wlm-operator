42c2dc2e6ed51d0463425db8dca5564fad3f2bbb
sashayakovtseva <sasha@sylabs.io>
2019-06-21 17:52:27 +0300

    Do not download operator modules

--- vagrant/install-common.sh
+++ vagrant/install-common.sh
@@ -39,13 +39,14 @@ export SYCRI_SERVICE=$(cat <<EOF
 [Unit]
 Description=Singularity-CRI daemon
 StartLimitIntervalSec=0
+
 [Service]
 Type=simple
 Restart=always
 RestartSec=30
 User=root
 Group=root
-ExecStart=/home/vagrant/go/src/github.com/sylabs/singularity-cri/bin/sycri
+ExecStart=/home/vagrant/singularity-cri/bin/sycri
 EOF
 )
 
@@ -55,20 +56,19 @@ SINGULARITY_CRI_REPO="https://github.com/sylabs/singularity-cri"
 SINGULARITY_WLM_OPERATOR_REPO="github.com/sylabs/wlm-operator"
 GOPATH="${HOME}/go"
 
-export DEBIAN_FRONTEND=noninteractive
-
-
 sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev libseccomp-dev pkg-config squashfs-tools
+sudo dpkg-reconfigure libc6
+DEBIAN_FRONTEND=noninteractive sudo dpkg --configure libssl1.1
 
-export VERSION=1.12 OS=linux ARCH=amd64
+export VERSION=1.12.6 OS=linux ARCH=amd64
 
 wget -q -O /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
 sudo tar -C /usr/local -xzf /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
+rm /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
 
 echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
 echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
-
 mkdir ${HOME}/go
 
 export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin
@@ -79,7 +79,7 @@ cd ${HOME}/singularity && ./mconfig && cd ./builddir &&  make && sudo make insta
 git clone ${SINGULARITY_CRI_REPO} ${HOME}/singularity-cri
 cd ${HOME}/singularity-cri && make && sudo make install
 
-go get ${SINGULARITY_WLM_OPERATOR_REPO}
+go get -d ${SINGULARITY_WLM_OPERATOR_REPO}
 
 sudo sh -c "printf '%s\n' '${SYCRI_SERVICE}' >> /etc/systemd/system/sycri.service"
 sudo systemctl start sycri
