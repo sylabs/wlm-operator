266a0a8afbe6820a2eb0bdce6041feec6c6de557
sashayakovtseva <sasha@sylabs.io>
2019-07-08 17:02:09 +0300

    Migrate to go modules

---
+++ tools.go
@@ -0,0 +1,13 @@
+//+build tools
+
+package wlm_operator
+
+import (
+	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/deepcopy-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/informer-gen"
+	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+)
